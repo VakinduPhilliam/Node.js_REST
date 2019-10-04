7cdd27f3b8445a2597e1b6bea6133987b142890b
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-04 23:07:23 +0300

    Add files via upload

---
+++ Node_Express_Postgresql/src/models/message.js
@@ -0,0 +1,25 @@
+const message = (sequelize, DataTypes) => {
+  
+
+const Message = sequelize.define('message', {
+ text: DataTypes.STRING,
+  });
+
+  
+
+Message.associate = models => {
+ Message.belongsTo(models.User);
+  
+
+};
+
+  
+
+return Message;
+
+
+};
+
+
+
+export default message;
