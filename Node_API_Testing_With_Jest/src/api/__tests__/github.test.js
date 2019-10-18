e6cf0cebab7ed7265b2ed933dccf5c62a241b24e
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-18 18:34:39 +0300

    Add files via upload

---
+++ Node_API_Testing_With_Jest/src/api/__tests__/github.test.js
@@ -0,0 +1,66 @@
+/* eslint-env jest */
+
+
+
+// This commands loads the mocked request.js as defined in __mocks__/request.js
+
+
+jest.mock('../request')
+
+
+
+const github = require('../github')
+
+
+
+// A simple example test
+
+
+describe('#getUser() using Promises', () => {
+  
+
+it('should load user data', () => {
+    
+
+return github.getUser('vnglst')
+    
+
+.then(data => {
+      
+
+expect(data).toBeDefined()
+      
+expect(data.entity.name).toEqual('Koen van Gilst')
+    
+
+})
+  
+
+})
+
+
+})
+
+
+
+// The exact same test using async/await
+
+
+describe('#getUser() using async/await', () => {
+  
+
+it('should load user data', async () => {
+    
+
+const data = await github.getUser('vnglst')
+    
+
+expect(data).toBeDefined()
+    
+expect(data.entity.name).toEqual('Koen van Gilst')
+  
+
+})
+
+
+})
