6620fc4d8cae543240a08fd1425363e8d5c51f1f
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-18 18:20:21 +0300

    Add files via upload

---
+++ Node_Authentication/middlewares/auth.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken');
+
+const config =  require('../config');
+
+
+
+const checkAuth = (req, res, next) => {
+	
+
+var token = req.headers['token'];
+	
+
+if (!token)
+		
+
+return res.status(403).send({ auth: false, message: 'No token provided.' });
+
+	
+
+jwt.verify(token, config.jwtSecret, (err, decoded) => {
+		
+
+if (err)
+			
+
+return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    
+
+req.user = {
+			
+
+login: decoded.login,
+			
+id: decoded.id
+		
+
+};
+    
+
+next();
+	
+
+});
+
+
+}
+
+
+
+module.exports = {
+ checkAuth
+ }
