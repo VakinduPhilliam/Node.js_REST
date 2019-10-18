e8809fda6a7a562a76b48777e9b9f5a12cc09027
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-18 18:21:29 +0300

    Add files via upload

---
+++ Node_Authentication/models/index.js
@@ -0,0 +1,63 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../db');
+
+
+
+const User = sequelize.define('user', {
+	
+
+login: Sequelize.STRING,
+	
+password: Sequelize.STRING,
+
+
+});
+
+
+
+const Order = sequelize.define('order', {
+	
+
+title: Sequelize.STRING,
+	
+date: {
+		
+
+type: Sequelize.DATE,
+		
+defaultValue: Sequelize.NOW
+	
+
+},
+	
+user_id: {
+		
+
+type: Sequelize.INTEGER,
+		
+references: {
+			
+
+model: User,
+			
+key: 'id'
+		
+
+}
+	
+
+}
+
+
+});
+
+
+
+User.hasMany(Order, {foreignKey: 'user_id'});
+
+
+
+module.exports = { User,
+ Order
+ }
