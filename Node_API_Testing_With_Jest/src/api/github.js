feda2a464587108706eb9893e0176c6130f358fb
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-18 18:37:29 +0300

    Add files via upload

---
+++ Node_API_Testing_With_Jest/src/api/github.js
@@ -0,0 +1,9 @@
+import request from './request'
+
+
+
+const getUser = user => request(`https://api.github.com/users/${user}`)
+
+
+
+export { getUser }
---
+++ Node_API_Testing_With_Jest/src/api/request.js
@@ -0,0 +1,7 @@
+const rest = require('rest')
+
+const mime = require('rest/interceptor/mime')
+
+
+
+export default rest.wrap(mime)
