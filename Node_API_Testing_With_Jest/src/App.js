6c35cb818d5f271cb7b66779ae1d7ce46d640eda
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-18 18:38:20 +0300

    Add files via upload

---
+++ Node_API_Testing_With_Jest/src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+import { getUser } from './api/github'
+
+
+
+const renderLine = (user, key) => <li key={key}><b>{key}</b>: {user[key]}</li>
+
+
+
+class App extends Component {
+  
+
+constructor (props) {
+    
+
+super(props)
+    
+
+this.state = { user: {} }
+  
+
+}
+
+  
+
+componentDidMount () {
+    
+
+getUser('vnglst').then(data => {
+      
+
+this.setState({ user: data.entity })
+    
+
+})
+  
+
+}
+
+  
+
+render () {
+    
+
+const { user } = this.state
+    
+
+return (
+ <div className='App'>
+ <ul style={{ listStyle: 'none' }}>
+  {
+            
+
+// Loop over the object keys and render each key
+            
+
+Object.keys(user).map(key => renderLine(user, key))
+          
+
+}
+        
+
+</ul>
+      
+
+</div>
+    
+
+)
+  
+
+}
+
+
+}
+
+
+
+export default App
