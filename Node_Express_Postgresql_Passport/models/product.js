15278e101558b0083582d01b3b7970767364ad4e
Vakindu Philliam <Pvakindu@gmail.com>
2019-10-19 12:21:24 +0300

    Add files via upload

---
+++ Node_Express_Postgresql_Passport/models/product.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+module.exports = (sequelize, DataTypes) => {
+  
+
+const Product = sequelize.define('Product', {
+    
+
+prod_name: DataTypes.STRING,
+    
+prod_desc: DataTypes.STRING,
+    
+prod_price: DataTypes.FLOAT
+  
+
+}, {});
+  
+
+Product.associate = function(models) {
+    
+
+// associations can be defined here
+  
+
+};
+  
+
+return Product;
+
+
+};
+
